c92208c67d70f73c71582646ff413e36f7fabdf6
Mashebasep <mashebasep@gmail.com>
2025-11-23 05:47:22 -0700

    Initial commit: Django project with DRF, token authenticatioon, and Book API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSetss):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
